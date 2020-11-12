eecea96d0ff07ffde411679d570b03809f239ed1
Tuncer Damat <tuncer.damat@gmail.com>
2020-11-12 23:20:17 +0100

    Adds footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import './App.css';
 
+import Footer from './components/Footer';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -46,6 +48,9 @@ class App extends React.Component {
                 </Nav>
               </Navbar.Collapse>
             </Navbar>
+            
+            <Footer />
+            
           </Container>
         </Router>
     );  
