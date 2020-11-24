96bedde359cd199cc3d4bd1e5c0d98734b23e868
Tuncer Damat <tuncer.damat@gmail.com>
2020-11-25 00:01:37 +0100

    Axios Post to send a contact mail
    [branch:master]

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -3,6 +3,7 @@ import Hero from "../components/Hero";
 import Content from "../components/Content";
 import Form from "react-bootstrap/Form";
 import {Button} from "react-bootstrap";
+import Axios from "axios";
 
  class ContactPage extends React.Component {
     
@@ -34,6 +35,29 @@ import {Button} from "react-bootstrap";
          this.setState({
              disabled: true,
          });
+         
+         Axios.post("http://localhost:3030/api/email", this.state)
+             .then( res => {
+                 if(res.data.success){
+                     this.setState({
+                         disabled: false,
+                         emailSent: true
+                     });    
+                 } else {
+                     this.setState({
+                         disabled: false,
+                         emailSent: false
+                     });
+                 }
+             })
+             .catch(err => {
+                 
+                 console.log(err);
+                 this.setState({
+                     disabled: false,
+                     emailSent: false
+                 })
+             })
      }
      
      render() {
